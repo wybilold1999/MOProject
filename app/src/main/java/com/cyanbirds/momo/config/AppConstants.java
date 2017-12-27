e366c4c4ef6ab3352de93810878868814cfc3e2e
wangyuebin <wangyb@ecqun.com>
2017-12-27 15:00:03 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/momo/config/AppConstants.java
+++ app/src/main/java/com/cyanbirds/momo/config/AppConstants.java
@@ -11,9 +11,9 @@ package com.cyanbirds.momo.config;
  */
 public class AppConstants {
 	
-//	public static final String BASE_URL = "http://120.76.54.8/MoLoveServer/";
+	public static final String BASE_URL = "http://120.76.54.8/MoLoveServer/";
 //	public static final String BASE_URL = "http://192.168.1.100/MoLoveServer/";
-	public static final String BASE_URL = "http://10.0.109.57:8080/MoLoveServer/";
+//	public static final String BASE_URL = "http://10.0.109.57:8080/MoLoveServer/";
 
 	/**
 	 * 密码加密密匙
