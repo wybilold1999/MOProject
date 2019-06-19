1fd1572da8eb8bb19da437b2e4bb3639a6eaee87
AnyWayGobin <395044952@qq.com>
2019-06-19 23:08:33 +0800

    Merge branch '1026' of /Users/wangyuebin/Android/AndroidProject/MOProject with conflicts.

--- app/src/main/java/com/cyanbirds/momo/config/AppConstants.java
+++ app/src/main/java/com/cyanbirds/momo/config/AppConstants.java
@@ -11,8 +11,8 @@ package com.cyanbirds.momo.config;
  */
 public class AppConstants {
 	
-	public static final String BASE_URL = "http://120.76.54.8/MoLoveServer/";
-//	public static final String BASE_URL = "http://192.168.1.106:8080/MoLoveServer/";
+//	public static final String BASE_URL = "http://120.76.54.8/MoLoveServer/";
+	public static final String BASE_URL = "http://192.168.1.103:8080/MoLoveServer/";
 
 	/**
 	 * 密码加密密匙
@@ -20,15 +20,20 @@ public class AppConstants {
 	public static final String SECURITY_KEY = "ABCD1234abcd5678";
 
 	/**
+	 * 请求位置的高德web api的key
+	 */
+	public static final String WEB_KEY = "d64c0240c9790d4c56498b152a4ca193";
+
+	/**
 	 *容联云IM
 	 */
-	public static String YUNTONGXUN_ID = "8a216da862a4b4880162a4e0affe0024";
-	public static String YUNTONGXUN_TOKEN = "d72a6d87c6133d3809fc3d90219639e2";
+	public static String YUNTONGXUN_ID = "8a216da8621834ef0162236bdaf3082d";
+	public static String YUNTONGXUN_TOKEN = "24d8b627364ef5a229d6f128c94bddc8";
 
 	/**
 	 * QQ登录的appid和appkey
 	 */
-	public static final String mAppid = "1106117001";
+	public static String mAppid = "1106117001";
 
 	/**
 	 * 微信登录
@@ -36,28 +41,29 @@ public class AppConstants {
 	public static String WEIXIN_ID = "wx879c39e44c646e7c";
 
 	/**
-	 * 微信登录
+	 * 微信支付
 	 */
 	public static String WEIXIN_PAY_ID = "wx879c39e44c646e7c";
 
 	/**
+	 * 短信
+	 */
+	public static final String SMS_INIT_KEY = "1e4fb1379a464";
+	public static final String SMS_INIT_SECRET = "703d4d94e91393b5e779f1c2b2d67db8";
+
+	/**
 	 * 小米推送appid
 	 */
-	public static final String MI_PUSH_APP_ID = "2882303761517581583";
+	public static String MI_PUSH_APP_ID = "2882303761517581583";
 	/**
 	 * 小米推送appkey
 	 */
-	public static final String MI_PUSH_APP_KEY = "5811758124583";
+	public static String MI_PUSH_APP_KEY = "5811758124583";
 
 	/**
 	 * 阿里图片节点
 	 */
 	public static final String OSS_IMG_ENDPOINT = "http://real-love-server.img-cn-shenzhen.aliyuncs.com/";
-
-	/**
-	 * 百度api定位
-	 */
-	public static final String BAIDU_LOCATION_API = "http://api.map.baidu.com/location/ip?ak=icuYvoz6ZrKLTKC6m53hquZUzZlHUypg&coor=bd09ll&ip=";
 
 	public static final String WX_PAY_PLATFORM = "wxpay";
 
@@ -66,15 +72,5 @@ public class AppConstants {
 	public static int CHAT_LIMIT = 0;
 
 	public static String BUGLY_ID = "f70599b373";
-
-	public static String CITY_WE_CHAT_RESP_CODE = "city_we_chat_resp_code";
-
-	public static String PAY_SUCCESS = "pay_success";
-
-	public static String PUB_DYNAMIC = "pub_dynamic";
-
-	public static String UPDATE_USER_INFO = "update_user_info";
-
-	public static final String CITY = "深圳市";
 
 }
